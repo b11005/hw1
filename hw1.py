842a7ba082e4b9f1f6a6b51386fbc8293c5835d6
b11005 <instantjoy3@gmail.com>
2015-06-03 12:20:08 +0900

    2015/6/3

---
+++ hw1.py
@@ -0,0 +1,29 @@
+""" This buffer is for notes you don't want to save, and for Lisp evaluation.
+ If you want to create a file, visit that file with C-x C-f,
+ then enter the text in that file's own buffer."""
+
+def anagram(strings):
+  strings=strings.lower()
+  strings=list(strings)
+  a=sorted(strings)
+  b=len(strings)
+  f=open("/usr/share/dict/words")
+  dic=f.read()
+  f.close()
+  line1=dic.split('\n')
+  d={}
+  for line in line1[:5]:
+    line=line.lower()
+    name=list(line)
+    line2=sorted(name)
+    line2="".join(line2)
+    d[line]=line2
+    print d
+    dic={}
+    for i in range(len(line2)):
+      dic.setdefault(line2[i],0)
+      dic[line2[i]]+=1
+      print dic
+
+strings="Hello"  
+print anagram(strings)
