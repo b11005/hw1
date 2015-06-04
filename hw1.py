a6564972eaf19ba6e5a6fcf4ae13266ca74a0aab
b11005 <instantjoy3@gmail.com>
2015-06-04 15:09:31 +0900

    complete to count each alphabet

--- hw1.py
+++ hw1.py
@@ -1,10 +1,10 @@
+
 def anagram(string):
   string=string.lower()
   string=string.strip()
   string=list(string)
   a=sorted(string)
   word=count(a)
-  print word
   key=word.keys()
   value=word.values()
   f=open("/usr/share/dict/words")
@@ -12,22 +12,19 @@ def anagram(string):
   f.close()
   line1=dic.split('\n')
   d={}
-  result=[]
   for line in line1:
     line=line.lower()
     string=list(line)
     line2=sorted(string)
     line2="".join(line2)
     d[line]=line2
-  d=sorted(d.items(), key=len, reverse=True)
+  d=sorted(d.items(), key=lambda x:len(x[1]), reverse=True)
   for i in d:
+    result=0
+    num=len(i[0])
     temp=count(i[1])
-    print temp
     for k,v in temp.items():
       if key==k and value>=v:
-        return k
-
- 
 
 
 
@@ -39,8 +36,6 @@ def count(word):
     d[word[i]]=d.get(word[i],0)+1
   return d
 
-def number(word):
-  return len(word)
 
 strings="Helloworld"
 print anagram(strings)
