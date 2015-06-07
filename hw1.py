57fe13f7c27dde9f94d7a56fb317cae108db9864
b11005 <instantjoy3@gmail.com>
2015-06-07 16:57:19 +0900

    add

--- hw1.py
+++ hw1.py
@@ -1,11 +1,10 @@
-
 def anagram(string):
   string=string.lower()
   string=string.translate(None," ")
-  print string
   string=list(string)
   a=sorted(string)
   word=count(a)
+  print word
   key=word.keys()
   value=word.values()
   num=len(value)
@@ -15,25 +14,42 @@ def anagram(string):
   line1=dic.split('\n')
   d={}
   for line in line1:
-    line=line.lower()
-    string=list(line)
-    line2=sorted(string)
+    if len(line)>16:
+      del line
+      continue
+    str=line.lower()
+    str=list(str)
+    line2=sorted(str)
     line2="".join(line2)
     d[line]=line2
   d=sorted(d.items(), key=lambda x:len(x[1]), reverse=True)
   for i in d:
-    result=0
+    result={}
     nums=len(i[0])
     temp=count(i[1])
+    total=0
     for k,v in temp.items():
       if k not in key:
+        result[k]=result.get(k,0)+1
         break
-      elif v>word[str(k)]:
+      elif v>word[k]:
+        result[k]=result.get(k,0)+1
         break
       else: 
-        result+=1
-    if result>=num:
-       return i[0]
+        result[k]=result.get(k,0)
+    for f,g in result.items():
+      if f not in key:
+        total=-100
+        break
+      elif g==1:
+        total=-10
+        break
+      else: total+=1
+    if total<=nums and total>0:
+      print count(i[0])
+      return i[0]
+      break
+
 
 
 def count(word):
@@ -42,6 +58,13 @@ def count(word):
     d[word[i]]=d.get(word[i],0)+1
   return d
 
-
-strings="Moon Starer"
+def gen_ran_str(chars=None):
+  import random
+  import string
+  if chars==None: 
+    chars = string.lowercase
+  return ''.join([random.choice(chars) for i in range(16)]) 
+  
+strings=gen_ran_str()
+print strings
 print anagram(strings)
